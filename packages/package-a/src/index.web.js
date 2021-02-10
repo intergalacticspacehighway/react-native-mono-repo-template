300935e38f3c5f073c3d57d81821b68c27458853
Nishan <nishanbende@gmail.com>
2021-02-10 08:50:36 +0530

    init

---
+++ packages/package-a/src/index.web.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Text } from 'react-native';
+export const A = () => {
+    return React.createElement(Text, null, "Hello world web");
+};
---
+++ packages/package-a/src/index.web.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export const A = () => {
+  return <Text>Hello world web</Text>;
+};
